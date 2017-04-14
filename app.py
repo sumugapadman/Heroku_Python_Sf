17563998b18960e4d1cf08a101b3c6b24ac69975
Sumuga Padman <sumugapadman@yahoo.in>
2017-04-14 16:33:41 +0530

    Flask API with Simple Salesforce
    Flask API with Simple Salesforce

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from simple_salesforce import Salesforce
+
+app = Flask(__name__)
+api = Api(app)
+
+#Initialize a connection with Salesforce
+sf = Salesforce(username=os.environ.get('SALESFORCE_USERNAME'), password=os.environ.get('SALESFORCE_PASSWORD'), security_token=os.environ.get('SALESFORCE_TOKEN'))
+
+class Account(Resource):
+    # Tell the api what are required fields to get a meaniful error while insert
+    parser = reqparse.RequestParser()
+    parser.add_argument('Ownerid',required=True, help="Ownerid is a required Field")
+
+    def get(self, name):
+        results = sf.query("SELECT Id, Name FROM Account WHERE Name LIKE '%"+name+"%'")
+        return results, 201
+
+    def post(self, name):
+        data = Account.parser.parse_args()
+        response = sf.Account.create({'Ownerid':data['Ownerid'],'Name':name})
+        return response, 201
+
+#http://herokuapp.cmm/account/Burlington_texttilles
+api.add_resource(Account, '/account/<string:name>')
+app.run(port=5000)
